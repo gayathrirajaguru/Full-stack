86f7848aacb1a3156a703640653ed2d545fe685e
gayathri <gayuguru111@gmail.com>
2024-07-16 11:20:51 +0530

    initalcommit

---
+++ ex.js
@@ -0,0 +1,9 @@
+const names=["hindu","gayu","helan"];
+const age=[20,20,21];
+for(var i=0;i<1;i++)
+{
+    for(var j=0;j<1;j++)
+    {
+        document.write(<tr><td>${names[0]}</td><td>${age[0]}</td></tr>);
+    }
+}
---
+++ tablejs.js
@@ -0,0 +1,17 @@
+document.write("<table ><thead><th>Bus No</th><th>Bus route</th></thead>");
+for(var i=0;i<1;i++)
+{
+    for(var j=0;j<1;j++)
+        {
+            document.write("<tr><td>3</td><td>cheyyar</td></tr>");
+            document.write("<tr><td>4</td><td>arcot</td></tr>");
+            document.write("<tr><td>1</td><td>vellor</td></tr>");
+            document.write("<tr><td>8</td><td>vandavasi</td></tr>");
+
+
+
+
+    
+    };
+};
+document.write("</table>");
