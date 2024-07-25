57239c2ad68367ba956ff349fd55d48d826064f4
gayathri <gayuguru111@gmail.com>
2024-07-25 12:37:07 +0530

    todo list updated

---
+++ todo.html/todolist.js
@@ -0,0 +1,29 @@
+var ul=document.getElementById("list.container");
+    var input=document.getElementById("input");
+    function add()
+    {
+        var listitems=document.createElement("li");
+        listitems.innerHTML=input.value+"<button onclick='change(this)'>edit</button>" +"<button onclick='deleteItem(event)'>delete</button>";
+        ul.append(listitems);
+
+    }
+    function deleteItem(event)
+    {
+      event.target.parentElement.remove();
+    }
+    var currentItem=null;
+    function change(event)
+    {
+
+      currentItem=event.parentElement;
+      var popup=document.getElementById("popup");
+      popup.style.display="block";
+    }
+    function update2(event)
+    {
+      var input2=document.getElementById("input2");
+      var inputvalue=input2.value;
+      currentItem.innerHTML=inputvalue+"<button onclick='remove(event)'>delect</button>"+"<button onclick='change(this)'>Edit</button>";
+      popup.style.display="none";
+        input2.value='';
+    }
